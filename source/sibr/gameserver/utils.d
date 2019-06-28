e715456a7b212ab41cf7283b32648b0990cd4ea3
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2019-06-28 16:36:12 -0400

    randInt util

--- source/sibr/gameserver/utils.d
+++ source/sibr/gameserver/utils.d
@@ -1,4 +1,18 @@
 module sibr.gameserver.utils;
 
+import std.random;
+import core.time;
+
 ///Millisecond timestamp
-auto millis() { import core.time; return convClockFreq(MonoTime.currTime.ticks, MonoTime.ticksPerSecond, 1000); }
+auto millis() {
+	return convClockFreq(MonoTime.currTime.ticks, MonoTime.ticksPerSecond, 1000);
+}
+
+private Random generator;
+shared static this() {
+	generator = Random(unpredictableSeed);
+}
+
+int randInt(int min, int max) {
+	return uniform!"()"(min, max, generator);
+}
