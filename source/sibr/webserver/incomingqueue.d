ae61a1e4fda954e638766baa6eeb00bb53e0d782
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2019-06-29 22:09:41 -0400

    socket message type alias

--- source/sibr/webserver/incomingqueue.d
+++ source/sibr/webserver/incomingqueue.d
@@ -1,8 +1,10 @@
 module sibr.webserver.incomingqueue;
 
+private alias messageType_t = string;
+
 ///A queue of incoming messages from clients.
 shared class IncomingQueue {
-	private string[][ushort] messages;///The queued messages
+	private messageType_t[][ushort] messages;///The queued messages
 
 	///Checks if there is a message available from the socket `id`.
 	bool messageAvailable(ushort id) {
@@ -10,12 +12,12 @@ shared class IncomingQueue {
 	}
 
 	///Adds a message to the queue. `id` is the id of the socket that sent the message.
-	void queueMessage(ushort id, string message) {
+	void queueMessage(ushort id, messageType_t message) {
 		messages[id] ~= message;
 	}
 
 	///Pops the next message off of {socket}'s queue. {socket} is the socket with the id `id`.
-	string nextMessage(ushort id) {
+	messageType_t nextMessage(ushort id) {
 		immutable message = messages[id][0];
 		messages[id] = messages[id][1 .. $];
 		return message;
