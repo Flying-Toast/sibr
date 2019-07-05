1c0b078ef6fbe4b8dc703b8b27429109599270b9
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2019-07-05 09:43:22 -0400

    remove coordinates from Collidables, use Location component coords

--- source/sibr/gameserver/geometry.d
+++ source/sibr/gameserver/geometry.d
@@ -1,56 +1,62 @@
 module sibr.gameserver.geometry;
 
-interface Collidable {
-	bool collidesWith(Collidable);
-}
+import sibr.gameserver.component;
 
-private bool rectangleCollidesWithCircle(Rectangle r, Circle c) {
+interface Collidable {}
+
+//NOTE: Collidable things don't directly have coordinates. The LocationComponent is used for the coords.
+
+private bool rectangleCollidesWithCircle(Rectangle r, LocationC locR, Circle c, LocationC locC) {
 	import std.algorithm : clamp;
 	import std.math : hypot;
 
-	float closestX = clamp(c.x, r.x - (r.width / 2.0), r.x + (r.width / 2.0));
-	float closestY = clamp(c.y, r.y - (r.height / 2.0), r.y + (r.height / 2.0));
+	float closestX = clamp(locC.x, locR.x - (r.width / 2.0), locR.x + (r.width / 2.0));
+	float closestY = clamp(locC.y, locR.y - (r.height / 2.0), locR.y + (r.height / 2.0));
 
-	return hypot(c.x - closestX, c.y - closestY) <= c.radius;
+	return hypot(locC.x - closestX, locC.y - closestY) <= c.radius;
 }
 
-class Rectangle : Collidable {
-	float width;
-	float height;
-	//coordinates are of center
-	float x;
-	float y;
-
-	bool collidesWith(Collidable thing_) {
-		if (auto thing = cast(Rectangle) thing_) {//rectangle-rectangle collision
-			immutable thisREdge = x + (width / 2.0);//x coord of right edge of this
-			immutable thisLEdge = x - (width / 2.0);
-			immutable thisTEdge = y - (height / 2.0);
-			immutable thisBEdge = y + (height / 2.0);
-			immutable thatREdge = thing.x + (thing.width / 2.0);//x coord of right edge of other rectangle
-			immutable thatLEdge = thing.x - (thing.width / 2.0);
-			immutable thatTEdge = thing.y - (thing.height / 2.0);
-			immutable thatBEdge = thing.y + (thing.height / 2.0);
+bool collides(Collidable a, LocationC locA, Collidable b, LocationC locB) {
+	if (auto rect = cast(Rectangle) a) {//rectangle-*
+		if (auto rect2 = cast(Rectangle) b) {//rectangle-rectangle
+			immutable rEdge = locA.x + (rect.width / 2.0);//x coord of right edge of rect
+			immutable lEdge = locA.x - (rect.width / 2.0);
+			immutable tEdge = locA.y - (rect.height / 2.0);
+			immutable bEdge = locA.y + (rect.height / 2.0);
+			immutable rEdge2 = locB.x + (rect2.width / 2.0);//x coord of right edge of rect2
+			immutable lEdge2 = locB.x - (rect2.width / 2.0);
+			immutable tEdge2 = locB.y - (rect2.height / 2.0);
+			immutable bEdge2 = locB.y + (rect2.height / 2.0);
 			if (
-				thisREdge > thatLEdge &&
-				thisLEdge < thatREdge &&
-				thisTEdge < thatBEdge &&
-				thisBEdge > thatTEdge
+				rEdge > lEdge2 &&
+				lEdge < rEdge2 &&
+				tEdge < bEdge2 &&
+				bEdge > tEdge2
 			) {
 				return true;
 			} else {
 				return false;
 			}
-		} else if (auto thing = cast(Circle) thing_) {//rect-circle
-			return rectangleCollidesWithCircle(this, thing);
-		} else {
-			assert(0, typeof(this).stringof~"-"~typeof(thing_).classinfo.name~" collision not implemented.");
+		} else if (auto circle = cast(Circle) b) {//rectangle-circle
+			return rectangleCollidesWithCircle(rect, locA, circle, locB);
+		}
+	} else if (auto circle = cast(Circle) a) {//circle-*
+		if (auto circle2 = cast(Circle) b) {//circle-circle
+			import std.math : hypot;
+			return hypot(locA.x - locB.x, locA.y - locB.y) <= circle.radius + circle2.radius;
+		} else if (auto rect = cast(Rectangle) b) {//circle-rectangle
+			return rectangleCollidesWithCircle(rect, locB, circle, locA);
 		}
 	}
 
-	this(float x, float y, float width, float height) {
-		this.x = x;
-		this.y = y;
+	assert(0, "Collision not implemented for these shapes.");
+}
+
+class Rectangle : Collidable {
+	float width;
+	float height;
+
+	this(float width, float height) {
 		this.width = width;
 		this.height = height;
 	}
@@ -58,56 +64,50 @@ class Rectangle : Collidable {
 
 class Circle : Collidable {
 	float radius;
-	float x;
-	float y;
 
-	bool collidesWith(Collidable thing_) {
-		if (auto thing = cast(Circle) thing_) {//circle-circle collision
-			import std.math : hypot;
-			return hypot(x - thing.x, y - thing.y) <= radius + thing.radius;
-		} else if (auto thing = cast(Rectangle) thing_) {//circle-rect
-			return rectangleCollidesWithCircle(thing, this);
-		} else {
-			assert(0, typeof(this).stringof~"-"~typeof(thing_).classinfo.name~" collision not implemented.");
-		}
-	}
-
-	this(float x, float y, float radius) {
-		this.x = x;
-		this.y = y;
+	this(float radius) {
 		this.radius = radius;
 	}
 }
 
 unittest {
-	Collidable a = new Circle(-3.76, 2.19, 3);
-	Collidable b = new Circle(-1.72, -3.07, 2);
-	assert(!a.collidesWith(b));
-	assert(!b.collidesWith(a));
+	Collidable a = new Circle(3);
+	LocationC locA = new LocationC(-3.76, 2.19);
+	Collidable b = new Circle(2);
+	LocationC locB = new LocationC(-1.72, -3.07);
+	assert(!collides(a, locA, b, locB));
+	assert(!collides(b, locB, a, locA));
 
-	a = new Circle(-5.4, -0.47, 3);
-	assert(a.collidesWith(b));
-	assert(b.collidesWith(a));
-
-	////////////
-
-	a = new Rectangle(18.3, 7.5, 8.7, 5.2);
-	b = new Rectangle(19.6, 6.0, 10, 5);
-	assert(a.collidesWith(b));
-	assert(b.collidesWith(a));
-
-	a = new Rectangle(220.7, 186.4, 20.0, 18.22);
-	assert(!a.collidesWith(b));
-	assert(!b.collidesWith(a));
+	a = new Circle(3);
+	locA = new LocationC(-5.4, -0.47);
+	assert(collides(a, locA, b, locB));
+	assert(collides(b, locB, a, locA));
 
 	////////////
 
-	a = new Circle(10.3, 10.3, 8.7);
-	b = new Rectangle(10.5, 10.9, 5, 5);
-	assert(a.collidesWith(b));
-	assert(b.collidesWith(a));
+	a = new Rectangle(8.7, 5.2);
+	locA = new LocationC(18.3, 7.5);
+	b = new Rectangle(10, 5);
+	locB = new LocationC(19.6, 6.0);
+	assert(collides(a, locA, b, locB));
+	assert(collides(b, locB, a, locA));
 
-	b = new Rectangle(105.2, 109.9, 5.21, 5.16);
-	assert(!a.collidesWith(b));
-	assert(!b.collidesWith(a));
+	a = new Rectangle(20.0, 18.22);
+	locA = new LocationC(220.7, 186.4);
+	assert(!collides(a, locA, b, locB));
+	assert(!collides(b, locB, a, locA));
+
+	////////////
+
+	a = new Circle(8.7);
+	locA = new LocationC(10.3, 10.3);
+	b = new Rectangle(5, 5);
+	locB = new LocationC(10.5, 10.9);
+	assert(collides(a, locA, b, locB));
+	assert(collides(b, locB, a, locA));
+
+	b = new Rectangle(5.21, 5.16);
+	locB = new LocationC(105.2, 109.9);
+	assert(!collides(a, locA, b, locB));
+	assert(!collides(b, locB, a, locA));
 }
