79818b2d57fbcab2c51ca6f53064a41d79a3d997
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2019-07-05 12:26:01 -0400

    mapmaker

---
+++ util/mapmaker/source/main.d
@@ -0,0 +1,20 @@
+import std.stdio;
+import std.file;
+import std.conv;
+import std.array;
+
+void main(string[] args) {
+	auto width = args[1].to!uint;
+	auto height = args[2].to!uint;
+
+	auto rawData = cast(ubyte[]) read("image.data");
+	auto data = uninitializedArray!(ubyte[][][])(height, width, 3);
+
+	for (auto y = 0; y < height; y++) {
+		for (auto x = 0; x < width; x++) {
+			auto rowSize = width * 4 * y;
+			auto firstItem = rowSize + x*4;
+			data[y][x] = [rawData[firstItem], rawData[firstItem+1], rawData[firstItem+2]];
+		}
+	}
+}
