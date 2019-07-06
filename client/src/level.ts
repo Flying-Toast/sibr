010d6fbbede2ed4d5ae1068f4032f28fd4da7915
Ian Huang <ianhuang46@gmail.com>
2019-07-06 04:02:04 -0400

    Added deterministic grass

--- client/src/level.ts
+++ client/src/level.ts
@@ -30,6 +30,17 @@ export class Tile {
     set variant(to: number) {
         this.parent.variants[this._index] = to;
     }
+
+    get inBounds(): boolean {
+        if (this.x<0 || this.x>=this.parent.width || this.y<0 || this.y>=this.parent.height) {
+            return false;
+        }
+        return true;
+    }
+
+    offset(dx: number, dy: number) {
+        return new Tile(this.parent, this.x+dx, this.y+dy);
+    }
 }
 
 export class Level {
@@ -67,9 +78,10 @@ export class Level {
         const lvl = new Level(game, w, h);
 
         for (const tileData of data.map) {
-            const tile = lvl.getTile(tileData.x, tileData.y);
+            const tile = lvl.tile(tileData.x, tileData.y);
             tile.tileID = this.tileCodes[tileData.texture];
         }
+
         return lvl;
     }
 
@@ -81,7 +93,7 @@ export class Level {
         return y*this.width + x;
     }
 
-    getTile(x: number, y: number): Tile {
+    tile(x: number, y: number): Tile {
         return new Tile(this, x, y);
     }
 
@@ -90,24 +102,99 @@ export class Level {
     }
 
     bakeDetails() {
-        // Put grass on the dirt, possibly compute lighting
+        for (var x=0; x<this.width; x++) {
+            for (var y=0; y<this.height; y++) {
+                this.tile(x,y).variant = this.groundTileVariant(this.tile(x,y));
+            }
+        }
+    }
+
+    isValid(t: Tile): boolean {
+        if (t.x<0 || t.x>=this.width || t.y<0 || t.y>=this.height) {
+            return false;
+        }
+        return true;
+    }
+
+    groundTileVariant(t: Tile): number {
+        // Code written at 4AM is the best code
+        var xBias = 0;
+        var yBias = 0;
+
+        const left = t.offset(-1, 0);
+        const right = t.offset(1, 0);
+        const up = t.offset(0, -1);
+        const down = t.offset(0, 1);
+
+        if (!left.inBounds) {
+            xBias -= 1;
+        } else if (left.tileID == t.tileID) {
+            xBias -= 1;
+        }
+
+        if (!right.inBounds) {
+            xBias += 1;
+        } else if (right.tileID == t.tileID) {
+            xBias += 1;
+        }
+
+        if (!up.inBounds) {
+            yBias -= 1;
+        } else if (up.tileID == t.tileID) {
+            yBias -= 1;
+        }
+
+        if (!down.inBounds) {
+            yBias += 1;
+        } else if (down.tileID == t.tileID) {
+            yBias += 1;
+        }
+
+        if (xBias == -1 && yBias == -1) {
+            return 0;
+        }
+        if (xBias == 0 && yBias == -1) {
+            return 1;
+        }
+        if (xBias == 1 && yBias == -1) {
+            return 2;
+        }
+        if (xBias == -1 && yBias == 0) {
+            return 3;
+        }
+        if (xBias == 0 && yBias == 0) {
+            return 4;
+        }
+        if (xBias == 1 && yBias == 0) {
+            return 5;
+        }
+        if (xBias == -1 && yBias == 1) {
+            return 6;
+        }
+        if (xBias == 0 && yBias == 1) {
+            return 7;
+        }
+        if (xBias == 1 && yBias == 1) {
+            return 8;
+        }
+        return 5;
     }
 
     render(): RenderTexture {
         const renderer = this.game.pixiApp.renderer;
-        const spriteTable = this.game.spriteTable;
 
         for (var x=0; x<this.width; x++) {
             for (var y=0; y<this.height; y++) {
-                const tile = this.getTile(x, y);
+                const tile = this.tile(x, y);
                 if (tile.tileID == 1) {
-                    const sprite = new Sprite(spriteTable.getTexture("dirt"));
+                    var sprite = new Sprite(this.game.getTexture("dirt_"+tile.variant));
                     sprite.x = x*32;
                     sprite.y = y*32;
-                    renderer.render(sprite, this.renderTex);
+                    renderer.render(sprite, this.renderTex, false);
                 }
             }
         }
+        
         return this.renderTex;
     }
 
